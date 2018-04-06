901815e088bbf93a25b76023de99e8b848e856be
George  Pianka <gpianka1@gmail.com>
2018-04-06 04:22:53 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,6 +50,21 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    if turn_count(board).odd?
+      token = "O"
+    else
+      token = "X"
+    end
+    move(board,index, token)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def
 
     
   
