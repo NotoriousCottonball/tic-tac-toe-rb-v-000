70d22cb9d46b788e2afec2d367b421f699cad8b6
George  Pianka <gpianka1@gmail.com>
2018-04-06 04:01:25 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,6 +21,15 @@ def input_to_index(input)
   input.to_i - 1 
 end
 
+def move(board, index, token)
+  board[index] = token
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " " || board[index] == "")
+end
+
+
 
     
   
