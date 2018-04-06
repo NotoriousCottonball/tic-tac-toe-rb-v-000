0dff0ce4f1855f13c444055dbf1183b6fc9bc77e
George  Pianka <gpianka1@gmail.com>
2018-04-06 04:04:30 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,7 +29,10 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " " || board[index] == "")
 end
 
-
+def valid_move?(board, index)
+  if index.between?(0,8) && !position_taken?(board, index)
+    true 
+  else 
 
     
   
